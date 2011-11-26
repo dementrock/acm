eb1413b446a56dcc765a565930eb6a10a8720981
Rocky Duan <dementrock@gmail.com>
2011-11-26 02:07:22 -0800

    Changed No.5 to 1 line

--- projecteuler/ruby/5.rb
+++ projecteuler/ruby/5.rb
@@ -1,8 +1,1 @@
-require 'prime'
-h = Hash.new(0)
-(1..20).each { |x|
-  Prime.prime_division(x).each { |x, y|
-    h[x] = h[x] < y ? y : h[x]
-  }
-}
-puts Prime.int_from_prime_division(h.to_a)
+puts (1..20).reduce { |x, y| x.lcm(y) }
